d37275a7a6a1e35e653d7b9aef5a97425263500f
apuiji <2330683684@qq.com>
2024-06-09 00:06:23 +0800

    a

--- 23tree.h
+++ 23tree.h
@@ -11,20 +11,65 @@ extern "C" {
 
 typedef struct {
   void *parent;
+  void *children[0];
   void *lchd;
   void *mchd;
   void *rchd;
+  void *data[3];
 } zlt23Tree;
 
 #define zlt23TreeMemb(p, m) zltMemb(p, zlt23Tree, m)
 
+// constructors begin
 static inline zlt23Tree zlt23TreeMake(const void *parent) {
   return (zlt23Tree) { .parent = parent };
 }
 
+typedef void zlt23TreeDtor(void *tree);
 
+void zlt23TreeClean(void *tree, zlt23TreeDtor *dtor);
+// constructors end
 
-void *zlt23TreeMostSide
+// iterators begin
+/// @param tree requires not null
+/// @param side 0 left, 2 right
+void *zlt23TreeMostSide(const void *tree, int side);
+
+static inline void *zlt23TreeMostLeft(const void *tree) {
+  return zlt23TreeMostSide(tree, 0);
+}
+
+static inline void *zlt23TreeMostRight(const void *tree) {
+  return zlt23TreeMostSide(tree, 2);
+}
+
+/// @param tree requires not null
+/// @param xy 0 NLR, 1 NRL
+void *zlt23TreeNXY(const void *tree, int xy);
+
+/// @param tree requires not null
+/// @param xy 0
+void *zlt23TreeXNY(const void *tree, int xy);
+
+/// @param tree requires not null
+/// @param xy 0 LRN, 1 RLN
+void *zlt23TreeXYN(const void *tree, int xy);
+// iterators end
+
+// find operations begin
+/// @return -2 goto left child, -1 found data[0], 0 goto middle child, 1 found data[1], 2 goto right child
+typedef int zlt23TreeCmpForFind(const void *a, const void *b);
+
+/// @param[out] tree found tree the data inside, requires not null, usually initialized by null
+void *zlt23TreeFind(void **tree, const void *root, zlt23TreeCmpForFind *cmp, const void *data);
+
+/// @param[out] tree found tree the data inside, requires not null, usually initialized by null
+/// @return insert slot
+void **zlt23TreeFindForInsert(void **tree, void *root, zlt23TreeCmpForFind *cmp, const void *data);
+// find operations end
+
+void zlt23TreeAfterInsert(void **root, void *tree);
+void zltRBTreeBeforeErase(void **root, void *tree);
 
 #ifdef __cplusplus
 
--- bitree.h
+++ bitree.h
@@ -37,21 +37,65 @@ void zltBiTreeClean(void *tree, zltBiTreeDtor *dtor);
 /// @param side -1 top, 0 left, 1 right
 void *zltBiTreeMostSide(const void *tree, int side);
 
+static inline void *zltBiTreeMostTop(const void *tree) {
+  return zltBiTreeMostSide(tree, -1);
+}
+
+static inline void *zltBiTreeMostLeft(const void *tree) {
+  return zltBiTreeMostSide(tree, 0);
+}
+
+static inline void *zltBiTreeMostRight(const void *tree) {
+  return zltBiTreeMostSide(tree, 1);
+}
+
 /// @param tree requires not null
 /// @param xy 0 NLR, 1 NRL
 void *zltBiTreeNXY(const void *tree, int xy);
 
+static inline void *zltBiTreeNLR(const void *tree) {
+  return zltBiTreeNXY(tree, 0);
+}
+
+static inline void *zltBiTreeNRL(const void *tree) {
+  return zltBiTreeNXY(tree, 1);
+}
+
 /// @param tree requires not null
-/// @param xy 0
+/// @param xy 0 LNR, 1 RNL
 void *zltBiTreeXNY(const void *tree, int xy);
+
+static inline void *zltBiTreeLNR(const void *tree) {
+  return zltBiTreeXNY(tree, 0);
+}
+
+static inline void *zltBiTreeRNL(const void *tree) {
+  return zltBiTreeXNY(tree, 1);
+}
 
 /// @param tree requires not null
 /// @param xy 0 LRN, 1 RLN
 void *zltBiTreeXYN(const void *tree, int xy);
+
+static inline void *zltBiTreeLRN(const void *tree) {
+  return zltBiTreeXYN(tree, 0);
+}
+
+static inline void *zltBiTreeRLN(const void *tree) {
+  return zltBiTreeXYN(tree, 1);
+}
 // iterators end
 
 /// @param tree requires not null and has child on side rightabout rotation
 void *zltBiTreeRotate(void *tree, bool right);
+
+static inline void *zltBiTreeLRot(void *tree) {
+  return zltBiTreeRotate(tree, false);
+}
+
+static inline void *zltBiTreeRRot(void *tree) {
+  return zltBiTreeRotate(tree, true);
+}
 
 // find operations begin
 typedef int zltBiTreeCmpForFind(const void *data, const void *tree);
