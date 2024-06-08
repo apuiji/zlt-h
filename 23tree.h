90ece8a9e4d0bda425062fe0d9ed4259270613f1
apuiji <2330683684@qq.com>
2024-06-08 19:18:00 +0800

    a

---
+++ 23tree.h
@@ -0,0 +1,27 @@
+#ifndef ZLT_23TREE_H
+#define ZLT_23TREE_H
+
+#include"xyz.h"
+
+#ifdef __cplusplus
+
+extern "C" {
+
+#endif
+
+typedef struct {
+  void *parent;
+  void *lchd;
+  void *mchd;
+  void *rchd;
+} zlt23Tree;
+
+void *zlt23TreeMostSide
+
+#ifdef __cplusplus
+
+}
+
+#endif
+
+#endif
