a531524946d241299dfe8a15d1725427b5c4d37b
apuiji <2330683684@qq.com>
2024-06-21 15:19:56 +0800

    a

--- io.hh
+++ io.hh
@@ -6,6 +6,11 @@ namespace zlt::io {
     return zltFwriteStr(dest, s);
   }
 
+  template<size_t N>
+  static inline size_t write(FILE *dest, const char (&s)[N]) noexcept {
+    return write(dest, string::make(s));
+  }
+
   struct CloseGuard {
     FILE *&file;
     CloseGuard(FILE *&file) noexcept: file(file) {}
