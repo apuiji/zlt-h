f5f9fdab4c354105ebd5f6295c57de4af6ae26f4
apuiji <2330683684@qq.com>
2024-06-21 22:12:49 +0800

    a

--- io.hh
+++ io.hh
@@ -2,15 +2,6 @@
 #include"string.hh"
 
 namespace zlt::io {
-  static inline size_t write(FILE *dest, String s) noexcept {
-    return zltFwriteStr(dest, s);
-  }
-
-  template<size_t N>
-  static inline size_t write(FILE *dest, const char (&s)[N]) noexcept {
-    return write(dest, string::make(s));
-  }
-
   struct CloseGuard {
     FILE *&file;
     CloseGuard(FILE *&file) noexcept: file(file) {}
@@ -18,4 +9,41 @@ namespace zlt::io {
       fclose(file);
     }
   };
+
+  static inline size_t read(void *dest, size_t size, FILE *src) noexcept {
+    return fread(dest, 1, size, src);
+  }
+
+  template<class T>
+  static inline size_t read(T *dest, size_t count, FILE *src) noexcept {
+    return fread(dest, sizeof(T), count, src);
+  }
+
+  template<class T>
+  static inline size_t read(T &dest, FILE *dest) noexcept {
+    return read(&dest, 1, src);
+  }
+
+  static inline size_t write(FILE *dest, void *src, size_t size) noexcept {
+    return fwrite(src, 1, size, dest);
+  }
+
+  template<class T>
+  static inline size_t write(FILE *dest, T *src, size_t count) noexcept {
+    return fwrite(src, sizeof(T), count, dest);
+  }
+
+  template<class T>
+  static inline size_t write(FILE *dest, T &&src) noexcept {
+    return write(dest, &src, 1);
+  }
+
+  static inline size_t writeStr(FILE *dest, String s) noexcept {
+    return zltFwriteStr(dest, s);
+  }
+
+  template<size_t N>
+  static inline size_t writeStr(FILE *dest, const char (&s)[N]) noexcept {
+    return write(dest, string::make(s));
+  }
 }
