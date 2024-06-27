eeed1c9cc971475edabaa4d704b0bc8fff37fb2c
apuiji <2330683684@qq.com>
2024-06-27 11:45:24 +0800

    a

---
+++ strbuild.h
@@ -0,0 +1,19 @@
+#ifndef ZLT_STR_BUILD_H
+#define ZLT_STR_BUILD_H
+
+#include"stack.h"
+#include"string.h"
+
+#include"ifcpp_begin.h"
+
+bool zltStrBuildWrite(zltStack *dest, zltStack *buf, const void *data, size_t size);
+
+static inline bool zltStrBuildWriteStr(zltStack *dest, zltStack *buf, zltString str) {
+  return zltStrBuildWrite(dest, buf, str.data, str.size);
+}
+
+bool zltStrBuildFlush(zltStack *dest, zltStack *buf);
+
+#include"ifcpp_end.h"
+
+#endif
